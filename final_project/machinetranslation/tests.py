ca568872233cf604e71e91acd0988699def76b91
dat nguyen <nguyenvdat257@gmail.com>
2022-03-08 15:42:31 +0700

    done final project

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,18 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french(''), '')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english(''), '')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+
+if __name__ == '__main__':
+    unittest.main()
